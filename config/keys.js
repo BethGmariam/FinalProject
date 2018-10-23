da722d4018894029dcf1f2d63446ccb57f7edc2e
Beth.BootCamp <beth.gmariam@gmail.com>
2018-10-22 22:58:14 -0400

    MVC folder structure updated

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,5 @@
 // our respective <dbuser> is first name all lowercase except the first initial
 // our respective <dbpassword> is finalproject1
 module.exports = {
-    mongoURI:"mongodb://<dbuser>:<dbpassword>@ds135233.mlab.com:35233/finalprojectdb"
+    mLabMongoURI:"mongodb://<dbuser>:<dbpassword>@ds135233.mlab.com:35233/finalprojectdb"
 }
