977908294ed863c2b6c814add7c5b7c22a8c0960
chrissydc <bychriscelle@gmail.com>
2018-11-03 12:33:03 -0400

    updated pages folder, app can now be loaded

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
-import Nav from "./components/Nav";
-import Wrapper from "./components/Wrapper";
+// import Wrapper from "./components/Wrapper";
 import Landing from "./pages/Landing";
 import RegisForm from "./pages/RegisForm";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-
-
+import Nav from "./components/Nav";
 
 import './App.css'
 
 const App = () => (
   <Router>
-    <Wrapper>
+      <div>
       <Nav />
       <Route exact path="/" component={Landing}/>
       <Route exact path="/registration" component={RegisForm} />
       <Route exact path="/dashboard" component={Dashboard} />
       <Route exact path="/login" component={Login} />
-    </Wrapper>
+      </div>
   </Router>
 );
 
