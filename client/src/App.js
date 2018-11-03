adffd9044debf450c5fbdb3f655252663205a44b
chrissydc <bychriscelle@gmail.com>
2018-11-03 12:59:32 -0400

    Header added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
-// import Wrapper from "./components/Wrapper";
+import Wrapper from "./components/Wrapper";
 import Landing from "./pages/Landing";
 import RegisForm from "./pages/RegisForm";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Nav from "./components/Nav";
+import Header from "./components/Header"
 
 import './App.css'
 
 const App = () => (
   <Router>
-      <div>
+      <Wrapper>
+      <Header>Gift Exchange</Header>
       <Nav />
       <Route exact path="/" component={Landing}/>
       <Route exact path="/registration" component={RegisForm} />
       <Route exact path="/dashboard" component={Dashboard} />
       <Route exact path="/login" component={Login} />
-      </div>
+      </Wrapper>
   </Router>
 );
 
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = props => (
+    <h1>{props.children}</h1>
+)
+
+export default Header
