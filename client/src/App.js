5c17720ee3306ed3a6fdbb5c11844ecaa6703b90
chrissydc <bychriscelle@gmail.com>
2018-11-03 11:56:43 -0400

    pages/components updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
-import NavTabs from "./pages/NavTabs";
+import Nav from "./components/Nav";
 import Landing from "./pages/Landing";
 import RegisForm from "./pages/RegisForm";
 import Login from "./pages/Login";
@@ -12,7 +12,7 @@ import './App.css'
 const App = () => (
   <Router>
     <div>
-      <NavTabs />
+      <Nav />
       <Route exact path="/" component={Landing}/>
       <Route exact path="/registration" component={RegisForm} />
       <Route exact path="/dashboard" component={Dashboard} />
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,11 +1,57 @@
 import React from "react";
+import { NavLink } from "react-router-dom";// changed from Link to NavLink to get activeStyle option for styling
 
 const Nav = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a className="navbar-brand" href="/">
-      React Reading List
-    </a>
-  </nav>
+  <ul className="nav nav-tabs">
+
+   <li className="nav-item">
+      <NavLink
+        to="/"
+        className={
+          window.location.pathname === "/" ? "nav-link active" : "nav-link"
+        }
+        activeStyle={{color:"green"}}
+      >
+        Home
+      </NavLink>
+    </li>
+    <li className="nav-item">
+      <NavLink
+        to="/registration"
+        className={
+          window.location.pathname === "/registration" ? "nav-link active" : "nav-link"
+        }
+        activeStyle={{color:"green"}}
+
+      >
+        Register
+      </NavLink>
+    </li>
+    <li className="nav-item">
+      <NavLink
+        to="/login"
+        className={
+          window.location.pathname === "/login" ? "nav-link active" : "nav-link"
+        }
+        activeStyle={{color:"green"}}
+      >
+       Login
+      </NavLink>
+    </li>
+    <li className="nav-item">
+      <NavLink
+        to="/dashboard"
+        className={
+          window.location.pathname === "/dashboard" ? "nav-link active" : "nav-link"
+        }
+        activeStyle={{color:"green"}}
+
+      >
+       Dashboard
+      </NavLink>
+    </li>
+
+  </ul>
 );
 
 export default Nav;
