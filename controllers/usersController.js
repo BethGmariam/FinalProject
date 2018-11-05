daee540a0fa6c76c073d82ae52bb706650ba2c2c
Beth.BootCamp <beth.gmariam@gmail.com>
2018-11-05 09:02:42 -0500

    file clean ups

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -36,54 +36,6 @@ module.exports = {
   }
 };
 
-// const User = require("../models").User;
-
-// module.exports = {
-
-//   loginFn: function(req,res){
-//     User.findOne({email: req.body.email}, function(err, user) {
-//       if (err) {
-//         console.log(err);
-//         return res.status(500).send();
-//       }
-
-//       if (!user) {
-//         console.log("user not found")
-//         return res.status(404).send();
-//       }
-
-//       user.comparePassword(req.body.password, function(err, isMatch) {
-//         if (isMatch && isMatch == true) {
-//           req.session.user = user;
-//           return res.status(200).send("loggedin");
-//         } else {
-//           return res.status(401).send("unauth");
-//         }
-//       });
-//     })
-//   },
-//   registerFn:function(req, res) {
-//       var email = req.body.email;
-//       var password = req.body.password;
-    
-//       var newUser = new User();
-//       newUser.email = email;
-//       console.log(email);
-//       newUser.password = password;
-    
-//       newUser.save(function(err,savedUser) {
-//         if (err) {
-//           console.log(err);
-//           return res.status(500).send();
-//         }
-//         return res.status(200).send();
-//       })
-    
-//   }
-
-
-// }
-
 
 
 
