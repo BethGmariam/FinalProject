5e1b7302f4a7ec10a886aa0e73917f6d0cece01a
Beth.BootCamp <beth.gmariam@gmail.com>
2018-11-06 18:54:09 -0500

    watsoncommentedout

--- server.js
+++ server.js
@@ -44,70 +44,70 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`server started on PORT: ${PORT}`));
 
 //watson personality analysis  logic
-const db = require("./models");
- var params1 = {};
- var userId = '';
- db.User.find()
- .then(function(res) {
-   var lastItem = res.length-1;
+// const db = require("./models");
+//  var params1 = {};
+//  var userId = '';
+//  db.User.find()
+//  .then(function(res) {
+//    var lastItem = res.length-1;
 
-      params1.textToAnalyze = res[lastItem].personality
-      userId = res[lastItem]._id
-      console.log(lastItem)
+//       params1.textToAnalyze = res[lastItem].personality
+//       userId = res[lastItem]._id
+//       console.log(lastItem)
     
-     watsonAnalysisFn(params1)
-    .then((results) => {
-      console.log(JSON.stringify(results, null, 2))
+//      watsonAnalysisFn(params1)
+//     .then((results) => {
+//       console.log(JSON.stringify(results, null, 2))
       
-      var tempStorage ={
-        Openness:results.personality[0].percentile,
-        Conscientiousness:results.personality[1].percentile,
-        Extraversion:results.personality[2].percentile,
-        Agreeableness:results.personality[3].percentile,
-        EmotionalRange:results.personality[4].percentile
+//       var tempStorage ={
+//         Openness:results.personality[0].percentile,
+//         Conscientiousness:results.personality[1].percentile,
+//         Extraversion:results.personality[2].percentile,
+//         Agreeableness:results.personality[3].percentile,
+//         EmotionalRange:results.personality[4].percentile
   
-      };
+//       };
 
 
-      var myquery = { _id: userId };
-      // console.log(userId)
-      var newvalues = {$set: {
-        Openness: tempStorage.Openness,
-        Conscientiousness: tempStorage.Conscientiousness,
-        Extraversion: tempStorage.Extraversion,
-        Agreeableness: tempStorage.Agreeableness,
-        EmotionalRange: tempStorage.EmotionalRange,
-        PersonalityAnalysed: true
-        } };
-      db.User.updateMany(myquery, newvalues, function(err, res) {
-      if (err) throw err;
-        //db.User.close();
-        });
-      })
-      .catch((error) => console.log(error.message));
+//       var myquery = { _id: userId };
+//       // console.log(userId)
+//       var newvalues = {$set: {
+//         Openness: tempStorage.Openness,
+//         Conscientiousness: tempStorage.Conscientiousness,
+//         Extraversion: tempStorage.Extraversion,
+//         Agreeableness: tempStorage.Agreeableness,
+//         EmotionalRange: tempStorage.EmotionalRange,
+//         PersonalityAnalysed: true
+//         } };
+//       db.User.updateMany(myquery, newvalues, function(err, res) {
+//       if (err) throw err;
+//         //db.User.close();
+//         });
+//       })
+//       .catch((error) => console.log(error.message));
      
 
-  });
+//   });
 
   
-// main watson text analysis function
-var watsonAnalysisFn = function(params) {
-  return new Promise(function (resolve, reject) {
-    // var res = {};
+// // main watson text analysis function
+// var watsonAnalysisFn = function(params) {
+//   return new Promise(function (resolve, reject) {
+//     // var res = {};
 
-    const PersonalityInsightsV3 =
-      require('watson-developer-cloud/personality-insights/v3');
-    var personalityInsights = new PersonalityInsightsV3({
-      version: '2017-10-13',
-      iam_apikey: 'Ubk285AL1s4RoiooAdNATd_0V03e8B8b2e0PZpELryGr',
-      url: 'https://gateway.watsonplatform.net/personality-insights/api'
-  });
+//     const PersonalityInsightsV3 =
+//       require('watson-developer-cloud/personality-insights/v3');
+//     var personalityInsights = new PersonalityInsightsV3({
+//       version: '2017-10-13',
+//       iam_apikey: 'Ubk285AL1s4RoiooAdNATd_0V03e8B8b2e0PZpELryGr',
+//       url: 'https://gateway.watsonplatform.net/personality-insights/api'
+//   });
 
-    personalityInsights.profile({'text': params.textToAnalyze},function(err, res) {
-      if (err)
-        reject(err);
-      else
-        resolve(res);
-    });
-  });
-}// end of watson personality analysis  logic
+//     personalityInsights.profile({'text': params.textToAnalyze},function(err, res) {
+//       if (err)
+//         reject(err);
+//       else
+//         resolve(res);
+//     });
+//   });
+// }// end of watson personality analysis  logic
