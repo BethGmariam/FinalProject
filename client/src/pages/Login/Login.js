81621211ad513f21288310e4b302fe71765a98fe
Beth.BootCamp <beth.gmariam@gmail.com>
2018-11-06 00:03:30 -0500

    consolidated everyone's pull request, fixed some typos like missing , in pkg.json, missing db in server.js

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -35,7 +35,8 @@ handleFormSubmit = (event)=>{
     .then(res => {
       if (true) {
         console.log("logged in");
-        this.state.loggedIn = true;
+        this.setState({loggedIn:true})
+        // this.state.loggedIn = true;
       }
     })
 }
