a5edd5561db0373a21a4bcaf11e7561000da3ffe
Beth.BootCamp <beth.gmariam@gmail.com>
2018-11-08 14:57:23 -0500

    test

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 require('mongoose-type-email');// to validate email address
-// const bcrypt = require('bcrypt');
-//   SALT_WORK_FACTOR = 10;
+
 
 var UserSchema = new mongoose.Schema({
 // fields created for login//
@@ -108,31 +107,5 @@ amountToSpend:{
 
         } // end of watsonAnalysis field
 })
-
-
-// UserSchema.pre('save', function(next) {
-//   var user = this;
-
-//   if(!user.isModified('password')) return next();
-
-//   bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-//     if (err) return next(err);
-
-//     bcrypt.hash(user.password, salt, function(err, hash) {
-//       if (err) return next(err);
-
-//       user.password = hash;
-//       next();
-//     });
-//   });
-// });
-
-
-// UserSchema.methods.comparePassword = function(candidatePassword, callback) {
-//     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-//       if (err) return callback(err);
-//       callback(undefined, isMatch);
-//     });
-// };
 
   module.exports = mongoose.model('User', UserSchema);
